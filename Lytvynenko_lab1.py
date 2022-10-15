91b6333f7b27a85085240b675a8f5b70cd4155f5
LytvynenkoJ <70521634+LytvynenkoJ@users.noreply.github.com>
2022-10-15 13:23:33 +0400

    Вивантажили інформацію з файлу

---
+++ Lytvynenko_lab1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+prob_data = pd.read_csv('prob_14.csv',delimiter=',', header=None)
+table_data = pd.read_csv('table_14.csv',delimiter=',', header=None)
+
+print(prob_data)
+print(table_data)
